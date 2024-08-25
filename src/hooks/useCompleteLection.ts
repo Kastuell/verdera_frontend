5ffd39decba8395f0f90889466883eb1c9a8932f
Kastuell <sholotun@mail.ru>
2024-08-26 01:51:25 +1000

    qwe

--- src/hooks/useCompleteLection.ts
+++ src/hooks/useCompleteLection.ts
@@ -1,19 +1,14 @@
 import { lectionService } from "@/services/lection.service";
 import { queryClient } from "@/utils/TanstackProvider";
 import { useMutation } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 export function useCompleteLection(slug: string) {
-  const { push, refresh } = useRouter();
-
   const { mutate, isPending } = useMutation({
     mutationKey: [`complete lection ${slug}`],
     mutationFn: () => lectionService.completeLection(slug),
     onSuccess: () => {
-      refresh();
       toast("Лекция завершена");
-      push("/courses");
     },
     onSettled: () =>
       queryClient.invalidateQueries({
