12934b2c6763fcfdf3e8146767a457833da791c1
Kastuell <sholotun@mail.ru>
2024-08-26 01:16:23 +1000

    qwe

--- src/app/(auth)/courses/test/result/page.tsx
+++ src/app/(auth)/courses/test/result/page.tsx
@@ -1,5 +1,10 @@
 import { Result } from "@/components";
+import { Metadata } from "next";
 import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Результаты теста",
+};
 
 export default function Page() {
   return (
