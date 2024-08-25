e10da46b5b62fae0bf5efcafb974e062b5ad2898
Kastuell <sholotun@mail.ru>
2024-08-26 01:12:03 +1000

    qwe

--- src/components/test/TestWrapper.tsx
+++ src/components/test/TestWrapper.tsx
@@ -9,86 +9,88 @@ import { Question } from "./Question";
 export const TestWrapper = ({ slug }: { slug: string }) => {
   const { data, isLoading, error } = useTest(slug);
 
-  const { mutate } = useCompleteTest();
-
-  const [curQuest, setQuest] = useState<number>(0);
-
-  const testLength = data?.questions.length;
-
-  const [answers, setAnswers] = useState<
-    { questId: number; answerId: number[] }[]
-  >([]);
-
-  const validToNext =
-    curQuest < (testLength ?? 10) - 1 && answers[curQuest] !== undefined;
-
-  const changeQuest = () => {
-    if (validToNext) setQuest((prev) => prev + 1);
-  };
-
-  const handleClick = () => {
-    console.log(answers);
-    mutate({
-      testId: data?.id,
-      userTest: answers,
-    });
-  };
-
-  const changeAnswers = (answer: {
-    questId: number;
-    answerId: number[];
-    isMulti?: boolean;
-  }) => {
-    const { answerId, questId, isMulti } = answer;
-    const inAnswersIndex = answers.findIndex(
-      (item) => item.questId == answer.questId
-    );
-    if (inAnswersIndex !== -1) {
-      const prevArr = answers;
-      if (isMulti) {
-        prevArr.splice(inAnswersIndex, 1, {
-          answerId: answerId,
-          questId: questId,
-        });
-      } else {
-        prevArr.splice(inAnswersIndex, 1, {
-          answerId: answerId,
-          questId: questId,
-        });
-      }
-    } else {
-      setAnswers((prev) =>
-        prev.concat({ answerId: answerId, questId: questId })
-      );
-    }
-  };
-
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Вам сюда рановато)</div>;
 
-  return (
-    <div className="xl:w-1/2 mx-auto">
-      <Head className="mt-5">
-        Вопрос {curQuest + 1} / {testLength}
-      </Head>
-      <Question
-        changeAnswers={changeAnswers}
-        question={data?.questions[curQuest]}
-      />
-      <div className="mt-10">
-        {curQuest !== (testLength ?? 0) - 1 ? (
-          <Button disabled={!validToNext} onClick={changeQuest}>
-            Далее
-          </Button>
-        ) : (
-          <Button
-            disabled={answers[curQuest] == undefined}
-            onClick={() => handleClick()}
-          >
-            Завершить тест
-          </Button>
-        )}
+  if (error) return <div>Вам сюда рановато)</div>;
+  if (data) {
+    const { mutate } = useCompleteTest();
+
+    const [curQuest, setQuest] = useState<number>(0);
+
+    const testLength = data.questions.length;
+
+    const [answers, setAnswers] = useState<
+      { questId: number; answerId: number[] }[]
+    >([]);
+
+    const validToNext =
+      curQuest < (testLength ?? 10) - 1 && answers[curQuest] !== undefined;
+
+    const changeQuest = () => {
+      if (validToNext) setQuest((prev) => prev + 1);
+    };
+
+    const handleClick = () => {
+      console.log(answers);
+      mutate({
+        testId: data.id,
+        userTest: answers,
+      });
+    };
+
+    const changeAnswers = (answer: {
+      questId: number;
+      answerId: number[];
+      isMulti?: boolean;
+    }) => {
+      const { answerId, questId, isMulti } = answer;
+      const inAnswersIndex = answers.findIndex(
+        (item) => item.questId == answer.questId
+      );
+      if (inAnswersIndex !== -1) {
+        const prevArr = answers;
+        if (isMulti) {
+          prevArr.splice(inAnswersIndex, 1, {
+            answerId: answerId,
+            questId: questId,
+          });
+        } else {
+          prevArr.splice(inAnswersIndex, 1, {
+            answerId: answerId,
+            questId: questId,
+          });
+        }
+      } else {
+        setAnswers((prev) =>
+          prev.concat({ answerId: answerId, questId: questId })
+        );
+      }
+    };
+
+    return (
+      <div className="xl:w-1/2 mx-auto">
+        <Head className="mt-5">
+          Вопрос {curQuest + 1} / {testLength}
+        </Head>
+        <Question
+          changeAnswers={changeAnswers}
+          question={data.questions[curQuest]}
+        />
+        <div className="mt-10">
+          {curQuest !== (testLength ?? 0) - 1 ? (
+            <Button disabled={!validToNext} onClick={changeQuest}>
+              Далее
+            </Button>
+          ) : (
+            <Button
+              disabled={answers[curQuest] == undefined}
+              onClick={() => handleClick()}
+            >
+              Завершить тест
+            </Button>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
