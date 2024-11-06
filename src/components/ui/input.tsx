b49d8b6d4032c096d36fdce59911e8c559bf6bbd
Kastuell <sholotun@mail.ru>
2024-11-06 17:50:40 +1000

    qwe

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -17,10 +17,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           {
             [`border-2 border-primary bg-background px-3 py-2 xl:h-20 h-16 placeholder:text-primary`]:
               !isProfile,
-              [`border-b-2 border-primary bg-transparent h-12 placeholder:text-primary lg:text-2xl`]:
-                isProfile,
-                [`hidden`]:
-                  type =="file",
+            [`border-b-2 border-primary bg-transparent h-12 placeholder:text-primary lg:text-2xl`]:
+              isProfile,
+            [`hidden`]: type == "file",
           },
           className
         )}
